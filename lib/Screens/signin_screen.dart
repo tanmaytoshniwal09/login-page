5de5df04b70a1cb03814cd63783b3c08e16ca380
tanmaytoshniwal8 <tanmaytoshniwal8@gmail.com>
2024-01-05 13:02:23 +0530

    Your commit message here

--- lib/Screens/signin_screen.dart
+++ lib/Screens/signin_screen.dart
@@ -21,9 +21,9 @@ class _SignInScreenState extends State<SignInScreen> {
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
             gradient: LinearGradient(colors: [
-          hexStringToColor("CB2B93"),
-          hexStringToColor("9546C4"),
-          hexStringToColor("5E61F4")
+          hexStringToColor("000000"),
+          hexStringToColor("323232"),
+          hexStringToColor("999999")
         ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
         child: SingleChildScrollView(
           child: Padding(
